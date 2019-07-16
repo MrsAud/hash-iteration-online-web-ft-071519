34eb9a513e88d10fb326dd4174d2d373c4622867
Audrea Cook <audreacwork@gmail.com>
2019-07-16 16:23:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,13 @@
 # birthday_kids = {
+#   kids_name ==> age
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts 
 end
 
 
